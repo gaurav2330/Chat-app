d41f8ffe2fb1b624acb0350dc81bc0eeb385db0f
Gaurav <gaurav@engagedly.com>
2023-05-28 19:36:14 +0530

    UI + socket events + location

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,28 @@
-io()
+const socket = io()
+
+socket.on('message', (message) => {
+  console.log(message);
+})
+
+socket.on('message-sent', (message) => {
+  document.getElementById('message-list').innerHTML += `<div class="card mb-2 p-2">${message}</div>`
+})
+
+document.getElementById('message-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  let message_box = document.getElementById('message')
+  let message_text = message_box.value
+  message_box.value = ''
+  socket.emit('send-message', message_text)
+})
+
+document.getElementById('send-location').addEventListener('click', () => {
+  // if(navigator.geolocation) console.log('in here');
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position);
+    socket.emit('send-location', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    })
+  });
+})
--- src/index.js
+++ src/index.js
@@ -12,8 +12,24 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+let count = 0;
+
+io.on('connection', (socket) => {
   console.log('Socket connected');
+
+  socket.broadcast.emit('message', 'A new user has joined')
+
+  socket.on('send-message', (message) => {
+    io.emit('message-sent', (message))
+  })
+
+  socket.on('send-location', (locationData) => {
+    io.emit('message', (`https://google.com/maps?q=${locationData.latitude},${locationData.longitude}`))
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user left')
+  })
 })
 
 server.listen(port, () => {
