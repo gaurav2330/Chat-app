11d05740e44d4c59e809206ed77edca8ede9ed6a
Gaurav <sgrb26@gmail.com>
2023-05-29 12:57:40 +0530

    location and message rendering changes

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,42 @@
 const socket = io()
 
+/*  HTML Events  */
+
+const $messageForm = document.getElementById('message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormTextarea = $messageForm.querySelector('textarea')
+const $locationBtn = document.getElementById('send-location')
+
+$messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  $messageFormInput.disabled = true
+
+  let message_text = $messageFormTextarea.value
+  $messageFormTextarea.value = ''
+  $messageFormTextarea.focus()
+  socket.emit('send-message', message_text, () => {
+    console.log('Message acknowledgement');
+    $messageFormInput.disabled = false
+  })
+})
+
+$locationBtn.addEventListener('click', (e) => {
+  $locationBtn.disabled = true
+  navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('send-location', {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    }, (message) => {
+      console.log(message);
+      $locationBtn.disabled = false
+    })
+  });
+})
+
+
+/*  Socket Events */
+
 socket.on('message', (message) => {
   console.log(message);
 })
@@ -8,21 +45,6 @@ socket.on('message-sent', (message) => {
   document.getElementById('message-list').innerHTML += `<div class="card mb-2 p-2">${message}</div>`
 })
 
-document.getElementById('message-form').addEventListener('submit', (e) => {
-  e.preventDefault()
-  let message_box = document.getElementById('message')
-  let message_text = message_box.value
-  message_box.value = ''
-  socket.emit('send-message', message_text)
-})
-
-document.getElementById('send-location').addEventListener('click', () => {
-  // if(navigator.geolocation) console.log('in here');
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position);
-    socket.emit('send-location', {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-    })
-  });
+socket.on('location-message', (url) => {
+  console.log(url);
 })
--- src/index.js
+++ src/index.js
@@ -2,29 +2,32 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
+const filter = new Filter()
 
 const port = process.env.PORT || 4999
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-let count = 0;
-
 io.on('connection', (socket) => {
   console.log('Socket connected');
 
   socket.broadcast.emit('message', 'A new user has joined')
 
-  socket.on('send-message', (message) => {
+  socket.on('send-message', (message, callback) => {
+    message = filter.clean(message)
     io.emit('message-sent', (message))
+    callback()
   })
 
-  socket.on('send-location', (locationData) => {
-    io.emit('message', (`https://google.com/maps?q=${locationData.latitude},${locationData.longitude}`))
+  socket.on('send-location', (locationData, callback) => {
+    io.emit('location-message', (`https://google.com/maps?q=${locationData.latitude},${locationData.longitude}`))
+    callback('Location Shared')
   })
 
   socket.on('disconnect', () => {
